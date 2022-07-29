da0c2d992250fde3a42b5c0eb627de18ac97c2a4
yunjae <hampel@naver.com>
2022-07-29 15:41:40 +0900

    프로그래머스 77484 set버전 추가

--- programmers77484.py
+++ programmers77484.py
@@ -14,7 +14,21 @@ def solution(lottos, win_nums):
     answer = rank[count + zeros], rank[count]       
     return answer
 
+def solution2(lottos, win_nums):
+    
+    answer = []
+    zeros = 0
+    rank = [6, 6, 5, 4, 3, 2, 1]
+    
+    zeros = lottos.count(0)
+    W    = set(win_nums)
+    mine = set(lottos)
+    common = len(W & mine)
+    
+    answer = rank[common + zeros], rank[common]       
+    return answer
+
 lottos   = [44, 1, 0, 0, 31, 25]
 win_nums = [31, 10, 45, 1, 6, 19]
-answer = solution(lottos, win_nums)
+answer = solution2(lottos, win_nums)
 print(answer)
